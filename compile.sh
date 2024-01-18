71c1897ea90e678fd7965462d45e35bc428764fb
verte <verte.fra@gmail.com>
2024-01-17 19:49:03 -0500

    updae flow

--- compile.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-APP_PATH=./dist/env-manager
-
-# Compile for Linux
-GOOS=linux GOARCH=amd64 go build -o "$APP_PATH"
-# Compile for macOS
-GOOS=darwin GOARCH=amd64 go build -o "$APP_PATH"
-
